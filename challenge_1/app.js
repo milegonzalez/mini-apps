11f192174b39bd212d71cd7e5e4a1ea6926be142
Milena Gonzalez <milenagonzalez@Milenas-MBP.home>
2018-11-27 13:03:40 -0500

    game completed

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,19 +4,6 @@ let playerTwo = "O";
 let player;
 let board;
 
-//Create an array with all possible winning solutions.
-let winningSolutions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-]
-
-
 //function newGame starts with an empty board that will be filled with X and O.
 let newGame = function () {
   let player = playerOne;
@@ -30,45 +17,85 @@ let newGame = function () {
 
 //determine if there is a winning board and stop people from continuing to play
 let winningBoard = function (board) {
-  let winningCombo = [];
-  for (let i = 0; i < board.length; i++) {
-    if (board[i] === 'X') {
-      winningCombo.push(i);
-    }
+  let winnerText = document.getElementsByClassName("scoreInfo");
+  let squares = document.getElementsByClassName("square");
+  //need to be refactored!!!!
+  //first row
+  if ("X" === squares[0].innerText && squares[1].innerText === squares[0].innerText && squares[2].innerText === squares[0].innerText) {
+    winnerText[0].innerText = "X Won";
   }
-  //its a bad algorithm, but its partially working.
-  for (var i = 0; i < winningSolutions.length; i++) {
-    // console.log('winningCombo',  winningCombo, 'winningSolutions[i]', winningSolutions[i])
-    if (winningCombo === winningSolutions[i]) {
-
-          console.log('player won!')
-//  document.getElementsByClassName('scoreInfo').innerText = 'X Won!'
-    }
+  if ("O" === squares[0].innerText && squares[1].innerText === squares[0].innerText && squares[2].innerText === squares[0].innerText) {
+    winnerText[0].innerText = "O Won";
+  }
+  //middle row
+  if ("X" === squares[3].innerText && squares[5].innerText === squares[3].innerText && squares[4].innerText === squares[3].innerText) {
+    winnerText[0].innerText = "X Won";
+  }
+  if ("0" === squares[3].innerText && squares[5].innerText === squares[3].innerText && squares[4].innerText === squares[3].innerText) {
+    winnerText[0].innerText = "0 Won";
+  }
+  //last row
+  if ("X" === squares[6].innerText && squares[7].innerText === squares[6].innerText && squares[8].innerText === squares[6].innerText) {
+    winnerText[0].innerText = "X Won";
+  }
+  if ("O" === squares[6].innerText && squares[7].innerText === squares[6].innerText && squares[8].innerText === squares[6].innerText) {
+    winnerText[0].innerText = "O Won";
+  }
+  //first column
+  if ("X" === squares[0].innerText && squares[3].innerText === squares[0].innerText && squares[6].innerText === squares[3].innerText) {
+    winnerText[0].innerText = "X Won";
+  }
+  if ("O" === squares[0].innerText && squares[3].innerText === squares[0].innerText && squares[6].innerText === squares[3].innerText) {
+    winnerText[0].innerText = "O Won";
+  }
+  //center column
+  if ("X" === squares[1].innerText && squares[4].innerText === squares[7].innerText && squares[1].innerText === squares[4].innerText) {
+    winnerText[0].innerText = "X Won";
+  }
+  if ("O" === squares[1].innerText && squares[4].innerText === squares[7].innerText && squares[1].innerText === squares[4].innerText) {
+    winnerText[0].innerText = "O Won";
+  }
+  //last column
+  if ("X" === squares[2].innerText && squares[2].innerText === squares[8].innerText && squares[5].innerText === squares[2].innerText) {
+    winnerText[0].innerText = "X Won";
+  }
+  if ("O" === squares[2].innerText && squares[2].innerText === squares[8].innerText && squares[5].innerText === squares[2].innerText) {
+    winnerText[0].innerText = "O Won";
+  }
+  //diagonal one
+  if ("X" === squares[0].innerText && squares[0].innerText === squares[4].innerText && squares[4].innerText === squares[8].innerText) {
+    winnerText[0].innerText = "X Won";
+  }
+  if ("O" === squares[0].innerText && squares[0].innerText === squares[4].innerText && squares[4].innerText === squares[8].innerText) {
+    winnerText[0].innerText = "O Won";
+  }
+  //diagonal two
+  if ("X" === squares[2].innerText && squares[2].innerText === squares[4].innerText && squares[4].innerText === squares[6].innerText) {
+    winnerText[0].innerText = "X Won";
+  }
+  if ("O" === squares[2].innerText && squares[2].innerText === squares[4].innerText && squares[4].innerText === squares[6].innerText) {
+    winnerText[0].innerText = "O Won";
+  } else if(board.indexOf(0) === -1) {
+    winnerText[0].innerText = "Its a Tie!";
   }
 }
 
 //need to define how to change players.
 let XorO = function (square) {
-  if(player === playerOne) {
+  var nextPlayerWording = document.getElementsByClassName("nextTurn");
+  if (player === playerOne) {
     document.getElementById(square.path[0].id).innerText = "X";
     board[square.path[0].id] = "X";
-    console.log(board);
-    document.getElementsByClassName("nextTurn").innerText = "Player Two!";
+    nextPlayerWording[0].innerText = "Next turn: Player O";
+    //rremove event listener from the square
     player = playerTwo;
   } else {
     document.getElementById(square.path[0].id).innerText = "O";
     board[square.path[0].id] = "O";
-    // document.getElementsByClassName("nextTurn").innerText = "Player One!";
+    nextPlayerWording[0].innerText = "Next turn: Player X";
     player = playerOne;
   }
   winningBoard(board);
 }
 
 newGame();
-
-
-let checkTiedGame = function (board) {
-  if (!winningBoard) {
-    document.getElementsByClassName('scoreInfo').innerText = 'Game Tied!';
-  }
-}
