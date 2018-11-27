641e0bf4aad57a3ba2108d871e835eb8b1b887d2
Milena Gonzalez <milenagonzalez@Milenas-MBP.home>
2018-11-27 14:30:18 -0500

    disabled board/square after player won

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -11,71 +11,104 @@ let newGame = function () {
   let squares = document.getElementsByClassName("square");
   for (let i = 0; i < squares.length; i++) {
     squares[i].innerText = '';
-    squares[i].addEventListener('click', XorO, false);
+    squares[i].addEventListener('click', XorO, true);
+  }
+}
+
+let disableSquare = function () {
+  let squares = document.getElementsByClassName("square");
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i].innerText === "X" || squares[i].innerText === "O") {
+      squares[i].removeEventListener('click', XorO, true);
+    }
+  }
+}
+
+let disableBoard = function () {
+  let squares = document.getElementsByClassName("square");
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].removeEventListener('click', XorO, true);
   }
 }
 
 //determine if there is a winning board and stop people from continuing to play
 let winningBoard = function (board) {
+  disableSquare();
   let winnerText = document.getElementsByClassName("scoreInfo");
   let squares = document.getElementsByClassName("square");
   //need to be refactored!!!!
   //first row
   if ("X" === squares[0].innerText && squares[1].innerText === squares[0].innerText && squares[2].innerText === squares[0].innerText) {
     winnerText[0].innerText = "X Won";
+    disableBoard();
   }
   if ("O" === squares[0].innerText && squares[1].innerText === squares[0].innerText && squares[2].innerText === squares[0].innerText) {
     winnerText[0].innerText = "O Won";
+    disableBoard();
   }
   //middle row
   if ("X" === squares[3].innerText && squares[5].innerText === squares[3].innerText && squares[4].innerText === squares[3].innerText) {
     winnerText[0].innerText = "X Won";
+    disableBoard();
   }
   if ("0" === squares[3].innerText && squares[5].innerText === squares[3].innerText && squares[4].innerText === squares[3].innerText) {
     winnerText[0].innerText = "0 Won";
+    disableBoard();
   }
   //last row
   if ("X" === squares[6].innerText && squares[7].innerText === squares[6].innerText && squares[8].innerText === squares[6].innerText) {
     winnerText[0].innerText = "X Won";
+    disableBoard();
   }
   if ("O" === squares[6].innerText && squares[7].innerText === squares[6].innerText && squares[8].innerText === squares[6].innerText) {
     winnerText[0].innerText = "O Won";
+    disableBoard();
   }
   //first column
   if ("X" === squares[0].innerText && squares[3].innerText === squares[0].innerText && squares[6].innerText === squares[3].innerText) {
     winnerText[0].innerText = "X Won";
+    disableBoard();
   }
   if ("O" === squares[0].innerText && squares[3].innerText === squares[0].innerText && squares[6].innerText === squares[3].innerText) {
     winnerText[0].innerText = "O Won";
+    disableBoard();
   }
   //center column
   if ("X" === squares[1].innerText && squares[4].innerText === squares[7].innerText && squares[1].innerText === squares[4].innerText) {
     winnerText[0].innerText = "X Won";
+    disableBoard();
   }
   if ("O" === squares[1].innerText && squares[4].innerText === squares[7].innerText && squares[1].innerText === squares[4].innerText) {
     winnerText[0].innerText = "O Won";
+    disableBoard();
   }
   //last column
   if ("X" === squares[2].innerText && squares[2].innerText === squares[8].innerText && squares[5].innerText === squares[2].innerText) {
     winnerText[0].innerText = "X Won";
+    disableBoard();
   }
   if ("O" === squares[2].innerText && squares[2].innerText === squares[8].innerText && squares[5].innerText === squares[2].innerText) {
     winnerText[0].innerText = "O Won";
+    disableBoard();
   }
   //diagonal one
   if ("X" === squares[0].innerText && squares[0].innerText === squares[4].innerText && squares[4].innerText === squares[8].innerText) {
     winnerText[0].innerText = "X Won";
+    disableBoard();
   }
   if ("O" === squares[0].innerText && squares[0].innerText === squares[4].innerText && squares[4].innerText === squares[8].innerText) {
     winnerText[0].innerText = "O Won";
+    disableBoard();
   }
   //diagonal two
   if ("X" === squares[2].innerText && squares[2].innerText === squares[4].innerText && squares[4].innerText === squares[6].innerText) {
     winnerText[0].innerText = "X Won";
+    disableBoard();
   }
   if ("O" === squares[2].innerText && squares[2].innerText === squares[4].innerText && squares[4].innerText === squares[6].innerText) {
     winnerText[0].innerText = "O Won";
-  } else if(board.indexOf(0) === -1) {
+    disableBoard();
+  } else if (board.indexOf(0) === -1) {
     winnerText[0].innerText = "Its a Tie!";
   }
 }
