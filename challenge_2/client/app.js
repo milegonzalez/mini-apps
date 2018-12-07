9c5a5b40c31e588495b7bf022729f9768cabfb5e
milegonzalez <milegonzalez@gmail.com>
2018-12-07 16:50:47 -0500

    working code, need to remove a comma

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,34 @@
 
+const postRequest = function (data) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:3000/postJSON',
+    data: { data: data },
+    success: function (data) {
+      $('.CSVdata').append(data);
+    },
+    error: function () {
+      console.log('there has been an error')
+    }
+  });
+}
+
+$('form').on('submit', function (e){
+  e.preventDefault()
+  let input = $(".textArea").val()
+  postRequest(input);
+})
+
+// this function selects the data from the text area and pushes
+// it to the <p>
+// $(".textArea")
+//   .keyup(function () {
+//     var value = $(this).val();
+//     postRequest(value)
+//     $("p").text(value);
+
+//   })
+//   .keyup();
+
+
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const port = 3000;
+
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}))
 
 
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+})
+
+app.post('/postJSON', function(req, res) {
+  console.log('this is req.body', req.body);
+
+  let recursiveFunc = function (nodes) {
+    let thread = [];
+
+    const recursive = function (node) {
+      for (var i in node) {
+        if (Array.isArray(node[i])) {
+          for (var j = 0; j < node[i].length; j++) {
+            recursive(node[i][j]);
+          }
+        } else if (typeof node[i] === 'number') {
+          thread.push(node[i] + '<br/>');
+        } else {
+          thread.push(node[i]);
+        }
+      }
+    }
+    var obj = JSON.parse(nodes);
+    recursive(obj)
+    let joinedThread = thread.join(",")
+    return joinedThread;
+  }
+
+  res.send(recursiveFunc(req.body.data))
+  res.status(200)
+});
+
+
+app.listen(port, function() {
+  console.log( `listening on port ${port}!`);
+});
